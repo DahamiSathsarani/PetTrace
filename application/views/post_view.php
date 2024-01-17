30b9d98b02bb628a294a6d68b3b4df750ae58e3f
KChathuranga <kavinduchathuranga12@gmail.com>
2024-01-17 16:50:23 +0530

    post page check

---
+++ application/views/post_view.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Post Page</title>
+</head>
+<body>
+    <h1>Post Page</h1>
+    <p>This is the post page of my CodeIgniter project.</p>
+</body>
+</html>
