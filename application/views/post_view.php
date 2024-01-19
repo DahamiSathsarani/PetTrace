5dbfcb4a1c57ba7913ce0952aa0c975824f710ae
KChathuranga <kavinduchathuranga12@gmail.com>
2024-01-19 12:38:15 +0530

    develop the post view page

--- application/views/post_view.php
+++ application/views/post_view.php
@@ -2,6 +2,18 @@
 
 <?php $this->load->view('header'); ?>
 
+<style>
+    body {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+    }
+
+    main {
+        flex: 1;
+    }
+</style>
+
 <!-- Nav Bar -->
 <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #6504b5;">
     <div class="container">
@@ -12,22 +24,43 @@
     </div>
 </nav>
 
+<!-- Main body -->
 <div class="container mt-5">
-    <h1>Bootstrap JS Example</h1>
+    <div class="row">
+        <!-- Image column -->
+        <div class="col-md-6">
+            <div class="card-image">
+                <img src="<?php echo base_url('assets/images/dog-2.jpg'); ?>" class="img-fluid" alt="Pet Image" width="500" height="200">
+            </div>
+        </div>
 
-    <!-- Bootstrap Button Triggering Alert -->
-    <button type="button" class="btn btn-primary" id="showAlertBtn">
-        Show Success Alert
-    </button>
-
-    <!-- Bootstrap Alert -->
-    <div class="alert alert-success mt-3" role="alert" id="successAlert" style="display: none;">
-        This is a success message!
+        <!-- Content column -->
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                <h2 class="mt-3">Pet Name</h2>
+                <ul>
+                    <li><strong>Breed:</strong> Labrador Retriever</li><br>
+                    <li><strong>Color:</strong> Brown</li><br>
+                    <li><strong>Description:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et urna eu lacus hendrerit fermentum.</li>
+                </ul>
+                <button class="btn btn-primary">Contact</button>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
+<!-- Footer -->
+<footer class="navbar navbar-expand-lg navbar-dark mt-auto" style="background-color: #6504b5; color: white;">
+    <div class="container">
+        <p>
+            
+        </p>
+    </div>
+</footer>
+
 <script>
-    // JavaScript to handle button click and show the success alert
     document.getElementById('showAlertBtn').addEventListener('click', function() {
         document.getElementById('successAlert').style.display = 'block';
     });
