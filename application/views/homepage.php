324a58fc2037aabfd1b649ae0450cf8339240348
Dahami Sathsarani <dahamisathsarani99@gmail.com>
2024-01-23 12:39:23 +0530

    Create footer section

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -4,7 +4,29 @@
 
 <style>
     #image{
-        height: 500px;
+        height: 520px;
+    }
+
+    .CategoryCard{
+        box-shadow: 0 0 10px gray;
+        transition: 0.1s;
+    }
+
+    .CategoryCard:hover{
+        border: solid 2px #6504b5;
+    }
+
+    .footer{
+        width: 100%;
+        height: 400px;
+        background-color: #6504b5;
+        display: flex;
+        justify-content: center;
+    }
+
+    i{
+        color: #fff;
+        font-size: 30px
     }
 
 </style>
@@ -49,11 +71,11 @@
 </div>
 
 <!-- Category section -->
-<!-- <div class="container mt-5">
+<div class="container mt-5">
     <div class="row">
         <?php foreach ($categories as $category): ?>
             <div class="col">
-                <div class="card CategoryCard" onclick="handleCategories(<?= $category['category_id']; ?>)">
+                <div class="card CategoryCard" href="http://localhost/PetTrace/index.php/Post/getCategorizedPost/<?= $category['category_id']; ?>">
                     <div class="card-body CategoryCardBody d-flex align-items-center justify-content-center">
                         <h5 class="card-title"><?= $category['category_name']; ?></h5>
                     </div>
@@ -61,7 +83,28 @@
             </div>
         <?php endforeach; ?>
     </div>
-</div> -->
+</div>
+
+<div class="footer mt-5">
+    <div class="row" style="">
+        <div class="col-md-6 text-light" style="width: 600px">
+            <h2 class="mt-5">PetTrace</h2>
+            <h5 class="mt-5">Contact Us</h5>
+            <p>Email: info@pettrace.com</p>
+            <p>Phone: +123 456 789</p>
+        </div>
+        <div class="col-md-6" style="width: 600px">
+            <a href="#" class=""><i class="bi bi-facebook"></i></a>
+            <a href="#" class=""><i class="bi bi-youtube"></i></a>
+            <a href="#" class=""><i class="bi bi-instagram"></i></a>
+        </div>
+    </div>
+    <div class="row" style="">
+        <div class="">
+            <p>&copy; 2024 PetTrace. All rights reserved.</p>
+        </div>
+    </div>
+</div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
