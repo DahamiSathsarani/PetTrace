ee61f8b2cf6c9e8170b3dfdeeba3d425bc3467c8
Dahami Sathsarani <dahamisathsarani99@gmail.com>
2024-01-23 12:40:28 +0530

    Link bootstrap icons

--- application/views/header.php
+++ application/views/header.php
@@ -10,8 +10,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
-    <!-- Font Awesome CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-B7zjPLfrJ+zUm+TkXc2BlxWPu4R1NT0KTUPPs8eFAvBykU2LFiF0eKJLx+SeYEbPXyxBLlBFrlM24ozq+DMyvLQ==" crossorigin="anonymous">
+    <!-- Bootstrap Icons CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 <body>
     
