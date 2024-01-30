7e71cf609ef9987575429bc19ff421a46a8a4a3e
Dahami Sathsarani <dahamisathsarani99@gmail.com>
2024-01-30 17:39:35 +0530

    Create posts section added by the user

--- application/views/user_view.php
+++ application/views/user_view.php
@@ -12,6 +12,12 @@
     main {
         flex: 1;
     }
+
+    .userImg{
+        width: 300px;
+        height: 300px;
+    }
+
 </style>
 
 <!-- Nav Bar -->
@@ -31,7 +37,7 @@
         <!-- Image column -->
         <div class="col-md-6">
             <div class="card-image" style="margin-left: 10%;">
-                <img src="<?= base_url('uploads/'. $userData['img_url']); ?>" class="" alt="User Profile Image" width="80%" height="80%">
+                <img src="<?= base_url('uploads/'. $userData['img_url']); ?>" class="userImg" alt="User Profile Image" width="80%" height="80%">
             </div>
             <br>
             <div class="card" style="width: 80%; padding-left:10px;">
@@ -51,6 +57,26 @@
         <div class="col-md-6">
             <div>
                 <h2>Posts</h2>
+                <div class="container mt-5">
+                <div class="row">
+                    <?php foreach ($userPosts as $post): ?>
+                        <div class="col-md-4 mb-4">
+                            <div class="card">
+                                <?php if (!empty($post->img_url)): ?>
+                                    <img src="<?= base_url('uploads/' . $post->img_url); ?>" class="card-img-top pet_image" alt="<?= $post->pet_name; ?>">
+                                <?php else: ?>
+                                    <img src="<?= base_url('path_to_default_image/default.jpg'); ?>" class="card-img-top" alt="Default Image">
+                                <?php endif; ?>
+                                <div class="card-body">
+                                    <h5 class="card-title"><?= $post->pet_name; ?></h5>
+                                    <p class="card-text"><?= $post->color; ?></p>
+                                    <p class="card-text"><?= $post->breed; ?></p>
+                                </div>
+                            </div>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+                </div>
             </div>
         </div>
     </div>
